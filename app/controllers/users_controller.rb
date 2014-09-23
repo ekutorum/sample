bbcc1bbc4926f6c66f23eea16e5336976fddc2fd
H Morales <ekutorum@gmail.com>
2014-09-23 23:05:08 +0000

    Letf off starting 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -18,7 +19,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
@@ -26,4 +39,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+  # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,10 @@ module SessionsHelper
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
-  
+
+    def current_user?(user)
+    user == current_user
+  end
 
   def sign_out
     current_user.update_attribute(:remember_token,
@@ -29,6 +32,13 @@ module SessionsHelper
     cookies.delete(:remember_token)
     self.current_user = nil
   end
-  
+    def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
   
 end
